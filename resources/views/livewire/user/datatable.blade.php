712b0a9c3b1883ae00bdc914dad159a941e7a656
Gabriel Zambrano <gab.zambrano@gmail.com>
2021-07-12 18:31:36 +0200

    file uploads improvements 4

--- resources/views/livewire/user/datatable.blade.php
+++ resources/views/livewire/user/datatable.blade.php
@@ -94,25 +94,27 @@
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
                             {{ $user->mobile }}
                         </td>
-                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 inline-flex">
-                            <span class="mr-1">{{ $user->work_status }}</span>
-                            @if ($user->work_status != 'working')
-                            @if ($user->work_status_verified == 1)
-                            <svg class="flex-shrink-0 h-5 w-5 text-green-500" viewBox="0 0 20 20" fill="currentColor"
-                                aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                            @else
-                            <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                            @endif
-                            @endif
+                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
+                            <div class="inline-flex">
+                                <span class="mr-1">{{ $user->work_status }}</span>
+                                @if ($user->work_status != 'working')
+                                @if ($user->work_status_verified == 1)
+                                <svg class="flex-shrink-0 h-5 w-5 text-green-500" viewBox="0 0 20 20"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                @else
+                                <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                    fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                @endif
+                                @endif
+                            </div>
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap flex justify-end items-center text-gray-400">
                             <a href="{{ route( 'users.edit', $user) }}" class="mx-1 hover:text-red-700">
@@ -130,7 +132,6 @@
                             </a>
                             @endif
                         </td>
-
                     </tr>
                     @endforeach
                 </tbody>
