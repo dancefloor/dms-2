2d6f6e2bbb6db2c12ff2559d7c0f234c20013017
Gabriel Zambrano <gab.zambrano@gmail.com>
2021-07-12 18:23:22 +0200

    file uploads improvements

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -113,6 +113,7 @@
                                     <td>
                                         <div class="inline-flex capitalize">
                                             <span class="mr-2">{{ $user->work_status }}</span>
+                                            @if ($user->work_status != 'working')
                                             @if ($user->work_status_verified == 1)
                                             <svg class="flex-shrink-0 h-5 w-5 text-green-500" viewBox="0 0 20 20"
                                                 fill="currentColor" aria-hidden="true">
@@ -131,6 +132,7 @@
                                                     d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
                                                     clip-rule="evenodd" />
                                             </svg> --}}
+                                            @endif
                                             @endif
                                         </div>
                                     </td>
@@ -158,9 +160,12 @@
                                         </div>
                                     </td>
                                     <td>
+                                        @if ($user->unemployement_proof)
                                         <a href="{{ asset('uploads/'. $user->unemployement_proof)}}" target="_blank"
                                             class="text-red-600 hover:underline">view
                                         </a>
+                                        @endif
+
                                     </td>
                                 </tr>
                             </table>
